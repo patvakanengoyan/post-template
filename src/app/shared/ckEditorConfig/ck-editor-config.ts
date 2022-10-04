310c68ceec4c187e9d46695cdec5c3f7c48fe00e
Pato <patvakan-engoyan@mail.ru>
2022-10-04 18:02:05 +0400

    add ckEditor config

---
+++ src/app/shared/ckEditorConfig/ck-editor-config.ts
@@ -0,0 +1,77 @@
+export const editorConfig = {
+
+  toolbar: {
+    items: [
+      'heading',
+      '|',
+      'bold',
+      'italic',
+      'link',
+      'bulletedList',
+      'numberedList',
+      '|',
+      'outdent',
+      'indent',
+      '|',
+      'alignment',
+      'imageUpload',
+      'blockQuote',
+      'insertTable',
+      'undo',
+      'redo',
+      'mediaEmbed'
+    ]
+  },
+  image: {
+    styles: [
+      'alignLeft', 'alignCenter', 'alignRight'
+    ],
+    resizeOptions: [
+      {
+        name: 'resizeImage:original',
+        label: 'Original',
+        value: null
+      },
+      {
+        name: 'resizeImage:50',
+        label: '25%',
+        value: '25'
+      },
+      {
+        name: 'resizeImage:50',
+        label: '50%',
+        value: '50'
+      },
+      {
+        name: 'resizeImage:75',
+        label: '75%',
+        value: '75'
+      }
+    ],
+    toolbar: [
+      'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
+      '|',
+      'ImageResize',
+      '|',
+      'imageTextAlternative'
+    ]
+  },
+  mediaEmbed: {
+    toolbar: [
+      'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
+      '|',
+      'ImageResize',
+      '|',
+      'imageTextAlternative'
+    ],
+    previewsInData: true
+  },
+  language: {
+    ui: 'en',
+    content: 'en',
+    textPartLanguage: [
+      { title: 'Arabic', languageCode: 'ar' },
+      { title: 'English', languageCode: 'en' }
+    ],
+  },
+};
--- src/app/views/posts/posts.component.ts
+++ src/app/views/posts/posts.component.ts
@@ -5,6 +5,7 @@ import {ModalDirective} from "ngx-bootstrap/modal";
 import {DeleteModalComponent} from "../../shared/utils/delete-modal/delete-modal.component";
 import {RequestService} from "../../shared/service/request.service";
 import {environment} from "../../../environments/environment.prod";
+import {editorConfig} from "../../shared/ckEditorConfig/ck-editor-config";
 
 @Component({
   selector: 'app-posts',
@@ -26,83 +27,7 @@ export class PostsComponent implements OnInit {
     status: ['', Validators.required],
   })
   public Editor = customBuild;
-  @Input() config = {
-
-    toolbar: {
-      items: [
-        'heading',
-        '|',
-        'bold',
-        'italic',
-        'link',
-        'bulletedList',
-        'numberedList',
-        '|',
-        'outdent',
-        'indent',
-        '|',
-        'alignment',
-        'imageUpload',
-        'blockQuote',
-        'insertTable',
-        'undo',
-        'redo',
-        'mediaEmbed'
-      ]
-    },
-    image: {
-      styles: [
-        'alignLeft', 'alignCenter', 'alignRight'
-      ],
-      resizeOptions: [
-        {
-          name: 'resizeImage:original',
-          label: 'Original',
-          value: null
-        },
-        {
-          name: 'resizeImage:50',
-          label: '25%',
-          value: '25'
-        },
-        {
-          name: 'resizeImage:50',
-          label: '50%',
-          value: '50'
-        },
-        {
-          name: 'resizeImage:75',
-          label: '75%',
-          value: '75'
-        }
-      ],
-      toolbar: [
-        'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
-        '|',
-        'ImageResize',
-        '|',
-        'imageTextAlternative'
-      ]
-    },
-    mediaEmbed: {
-      toolbar: [
-        'imageStyle:alignLeft', 'imageStyle:alignCenter', 'imageStyle:alignRight',
-        '|',
-        'ImageResize',
-        '|',
-        'imageTextAlternative'
-      ],
-      previewsInData: true
-    },
-    language: {
-      ui: 'en',
-      content: 'en',
-      textPartLanguage: [
-        { title: 'Arabic', languageCode: 'ar' },
-        { title: 'English', languageCode: 'en' }
-      ],
-    },
-  };
+  @Input() config = editorConfig;
   @ViewChild('autoShownModal', { static: false }) autoShownModal?: ModalDirective;
   @ViewChild(DeleteModalComponent) private modal!: DeleteModalComponent;
   isModalShown = false;
