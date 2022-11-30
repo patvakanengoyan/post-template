e3fbe8025e080a2b6b75646637f170db986c0351
Ara <ara190195@mail.ru>
2022-11-30 16:54:16 +0400

    google fonts create #3

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -91,25 +91,25 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Hero Slider
    */
-  var swiper = new Swiper(".sliderFeaturedPosts", {
-    spaceBetween: 0,
-    speed: 500,
-    centeredSlides: true,
-    loop: true,
-    slideToClickedSlide: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".custom-swiper-button-next",
-      prevEl: ".custom-swiper-button-prev",
-    },
-  });
+  // var swiper = new Swiper(".sliderFeaturedPosts", {
+  //   spaceBetween: 0,
+  //   speed: 500,
+  //   centeredSlides: true,
+  //   loop: true,
+  //   slideToClickedSlide: true,
+  //   autoplay: {
+  //     delay: 3000,
+  //     disableOnInteraction: false,
+  //   },
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     clickable: true,
+  //   },
+  //   navigation: {
+  //     nextEl: ".custom-swiper-button-next",
+  //     prevEl: ".custom-swiper-button-prev",
+  //   },
+  // });
 
   /**
    * Open and close the search form.
